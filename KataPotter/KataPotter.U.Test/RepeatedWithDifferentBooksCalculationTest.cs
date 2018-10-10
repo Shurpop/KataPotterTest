afbb713ba7ea7ee4b341ae6bce1c9d8c52a6276a
Shurpop <marcos.varela.88@gmail.com>
2018-10-11 00:17:50 +0200

    Added calculation of bundle of two groups of same book (calculation is incorrect)

--- KataPotter/KataPotter.U.Test/RepeatedWithDifferentBooksCalculationTest.cs
+++ KataPotter/KataPotter.U.Test/RepeatedWithDifferentBooksCalculationTest.cs
@@ -29,5 +29,25 @@ namespace KataPotter.U.Test
 
             Assert.Equal(23.2m, price);
         }
+
+        [Fact]
+        public void DiscountOf_TwoEqualBooks_Plus_TowEqualsBooks()
+        {
+            // Arrange
+            List<Book> books = new BooksBuilder()
+                                 .AddBook(BookType.FirstBook)
+                                 .AddBook(BookType.FirstBook)
+                                 .AddBook(BookType.SecondBook)
+                                 .AddBook(BookType.SecondBook);
+            //Act
+            var price = _booksCalculation.GetPriceBooks(books);
+
+            //assert 2 * (8 * 2 * 0.95)
+            //var dec = 2 * (8 * 2 * 0.95);
+
+            Assert.Equal(30.4m, price); 
+            //Calculation of groups by type is incorrect :(
+        }
+
     }
 }
